07d58f3140f4a1d5b7a3d8dd0af33e2823d40aae
Namukk <skadnr625@gmail.com>
2020-09-22 12:00:40 +0900

    day 1 -start

---
+++ src/Components/Router.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Home from "../Routes/Home";
+import TV from "../Routes/TV";
+import Search from "../Routes/Search";
+
+export default () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      {/* exact를 붙여줘야 정확히 일치하는 것만 인정. ex) /login, /course, /join 다 /로 인식됨. */}
+      <Route path="/tv" component={TV} />
+      {/* <Route path= "/tv/popular" render={() => <h1>Popular</h1>} />을 이용하면 두개 이상의 Route 랜더링 가능 = Composition */}
+      <Route path="/search" component={Search} />
+      <Redirect from="*" to="/" />
+      {/* 일치하는 route가 하나도 없을시(이상한 route로 넘어가면) home으로 redirect 시켜줌. // switch는 한번에 하나의 route만 렌더링(두개의 route 한번에 렌더링 되는 것 방지.) */}
+    </Switch>
+  </Router>
+);
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App"; // .env 1번 줄이 src폴더부터 탐색하게 만들었기 때문에 그냥 "Components/App" 해도 됨
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
